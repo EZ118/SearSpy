26a7ff8cae6e9205a2df80669206a6e0722eee02
ZZY_WISU <79049368+EZ118@users.noreply.github.com>
2023-06-01 17:30:09 +0800

    v1.6 update
    1. colored output mode (customizable)
    2. display-all mode (customizable)

--- README.md
+++ README.md
@@ -19,11 +19,13 @@ Type the website's URL you want to search in.
 Type your keyword (a word or a phrase is suggested)   
 Type the Recursion Depth (author suggested to fill the number between 10 and 20, larger number is okey but useless)   
 Type `y` or `Y` if you want to save all crawled links   
-Type `y` or `Y` to enable URL deduplication mode (TIP: It's faster, but it needs larger RAM)   
+Type `y` or `Y` to display all outputs   
    
 2. /tools/csv2json.py   
 Drag your csv file on its icon and release, or use `python3 cv2json.py <path>`, or run it and input the file path.   
    
 ## To Do
-1. csvTool to enrich the crawled data
-2. make it faster (multi-thread)
+1. support commond line parameter
+2. csvTool to enrich the crawled data
+3. make it faster (multi-thread)
+4. make the code format more standardized
--- SearSpy.py
+++ SearSpy.py
@@ -1,22 +1,41 @@
+VERSION = "v1.6"
+
+SaveAll_Flag = False        #全部保存爬虫记录
+UrlDeDuplication = True     #URL去重
+IfShowAll_Flag = False      #全部输出模式
+ColorOutput = True          #彩色输出
+
+LoadedUrl = []              #用于存储所有已请求过的URL，实现URL去重功能
+
+RequestTimeout = 10         #单次请求超时设置
+RequestHeaders = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Cookie": "token=c4d038b4bed09fdb1471ef51ec3a32cd; v=114514md5",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+}                           #请求头
+
+
 import requests
 import csv
 import re
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-SaveAll_Flag = False           #全部保存爬虫记录
-UrlDeDuplication = False    #URL去重
-LoadedUrl = []
 
-payload = ""
-#请求头
-headers = {
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-    "Cookie": "token=ee7e1e1e7dbe7; v=0",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-}
+
+#判断是否使用彩色输出
+if ColorOutput == True:
+    from colorama import init, Fore, Back, Style
+    init()
+else:
+    class Fore:
+        GREEN = WHITE = RED = ""
+    class Style:
+        RESET_ALL = ""
+
+
 
 def SaveCSV(url, current_url):
     #保存在CSV中
@@ -55,13 +74,14 @@ def getUrls(url, html_source):
                 links.append(hf)
         else:
             links.append(ToFullPath(url, hf))
-            #print("[ * ]  DEBUG: UrlLog-" + ToFullPath(url, hf))
+            #print(Fore.WHITE + "[ * ]  DEBUG: UrlLog-" + ToFullPath(url, hf))
             
     new_urls += links
     return new_urls
 
 def web_crawl(urls, key, depth):
-    print("[ * ]  DEBUG: Depth-" + str(depth))
+    #爬虫主函数
+    print(Fore.WHITE + "[ * ]  DEBUG: Depth-" + str(depth))
     all_Url = []
     if depth == 0:
         return;
@@ -73,14 +93,16 @@ def web_crawl(urls, key, depth):
         html_source = ""
         try:
             #请求URL
-            resp = requests.get(url, headers=headers)
+            resp = requests.get(url, headers = RequestHeaders, timeout = RequestTimeout)
             resp.encoding = resp.apparent_encoding
             html_source = resp.text
             
             #如果网页内容匹配
             if key in html_source:
                 SaveCSV(url, depth)
-                print("[ + ]  FOUND: Depth-" + str(depth) + "  Url-" + url)
+                print(Fore.GREEN + "[ + ]  FOUND: Depth-" + str(depth) + "  Url-" + url)
+            elif IfShowAll_Flag == True:
+                print(Fore.WHITE + "[ - ]  UNMATCH: Depth-" + str(depth) + "  Url-" + url)
 
             #如果开启记录所有爬取的URL模式
             if SaveAll_Flag == True:
@@ -94,7 +116,7 @@ def web_crawl(urls, key, depth):
             all_Url += UrlList
             
         except Exception as e:
-            print("[ - ]  ERROR: Depth-" + str(depth) + "  Url-" + url)
+            print(Fore.RED + "[ - ]  ERROR: Depth-" + str(depth) + "  Url-" + url)
 
     web_crawl(all_Url, key, depth - 1)
 
@@ -107,29 +129,31 @@ if __name__ == '__main__':
       ___) |  __/ (_| | |   ___) | |_) | |_| |
      |____/ \___|\__,_|_|  |____/| .__/ \__, |
                                  |_|    |___/ 
-    SearSpy Tool
+    SearSpy Tool ''' + VERSION + '''
     ''')
     
     url = input('Please enter the target link: ')
     key = input('Please enter the keyword: ')
     depth = int(input('Please enter the maximum recursion depth: '))
-    IfSaveAllUrl = input('Do you want to save all crawled links? [y/N]')
-    IfDeDuplication = input('Do you want to enable URL deduplication mode? (Faster speed, but larger RAM required) [y/N]')
+    IfSaveAllUrl = input('Do you want to save all crawled links? [y/N] ')
+    IfShowAll = input('Do you want to display all outputs? [y/N] ')
     
-    if IfSaveAllUrl == "y" or IfSaveAllUrl == "Y":
+    if IfSaveAllUrl.lower() == "y":
         SaveAll_Flag = True
     else:
         SaveAll_Flag = False
 
-    #是否开启URL去重
-    if IfDeDuplication == "y" or IfDeDuplication == "Y":
-        UrlDeDuplication = True
+    #是否开启调试模式
+    if IfShowAll.lower() == "y":
+        IfShowAll_Flag = True
     else:
-        UrlDeDuplication = False
+        IfShowAll_Flag = False
     
-    print("[ * ]  MSG: Loading...")
+    print("[ * ]  M S G: Loading...")
 
     # 开始执行爬虫
     url = [url]
     web_crawl(url, key, depth)
-    print("[ * ]  Done!")
+
+    print(Style.RESET_ALL)
+    print("[ * ]  M S G: Done!")
